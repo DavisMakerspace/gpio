9f7814c5d5d8d4798c102008055ccf1b2903e335
Braden Pellett <contact@dabrado.net>
2014-02-17 13:03:14 -0800

    Condense code

--- gpio.rb
+++ gpio.rb
@@ -6,60 +6,24 @@ class GPIO
   [:value, :direction, :edge, :active_low].each do |attr|
     define_method("#{attr}_path"){ "#{GPIO_PATH}/gpio#{@id}/#{attr}" }
   end
-  def exported?
-    File.exists?(value_path)
-  end
-  def export
-    File.write("#{GPIO_PATH}/export", @id); self
-  end
-  def unexport
-    File.write("#{GPIO_PATH}/unexport", @id); self
-  end
-  def input?
-    File.read(direction_path).chomp == 'in'
-  end
-  def output?
-    File.read(direction_path).chomp == 'out'
-  end
-  def set_input
-    File.write(direction_path, 'in'); self
-  end
-  def set_output(value = :low)
-    File.write(direction_path, value == :low ? 'low':'high'); self
-  end
-  def set?
-    File.read(value_path).chomp == '1'
-  end
-  def clear?
-    File.read(value_path).chomp == '0'
-  end
-  def set(v = true)
-    File.write(value_path, v ? '1':'0'); self
-  end
-  def clear
-    set false
-  end
-  def edge
-    File.read(edge_path).chomp.to_sym
-  end
-  def set_edge_none
-    File.write(edge_path, 'none'); self
-  end
-  def set_edge_rising
-    File.write(edge_path, 'rising'); self
-  end
-  def set_edge_falling
-    File.write(edge_path, 'falling'); self
-  end
-  def set_edge_both
-    File.write(edge_path, 'both'); self
-  end
-  def active_low?
-    File.read(active_low_path).chomp == '1'
-  end
-  def set_active_low(v = true)
-    File.write(active_low_path, v ? '1':'0'); self
-  end
+  def exported?; File.exists?(value_path); end
+  def export; File.write("#{GPIO_PATH}/export", @id); self; end
+  def unexport; File.write("#{GPIO_PATH}/unexport", @id); self; end
+  def input?; File.read(direction_path).chomp == 'in'; end
+  def output?; File.read(direction_path).chomp == 'out'; end
+  def set_input; File.write(direction_path, 'in'); self; end
+  def set_output(value = :low); File.write(direction_path, value == :low ? 'low':'high'); self; end
+  def set?; File.read(value_path).chomp == '1'; end
+  def clear?; File.read(value_path).chomp == '0'; end
+  def set(v = true); File.write(value_path, v ? '1':'0'); self; end
+  def clear; set false; end
+  def edge; File.read(edge_path).chomp.to_sym; end
+  def set_edge_none; File.write(edge_path, 'none'); self; end
+  def set_edge_rising; File.write(edge_path, 'rising'); self; end
+  def set_edge_falling; File.write(edge_path, 'falling'); self; end
+  def set_edge_both; File.write(edge_path, 'both'); self; end
+  def active_low?; File.read(active_low_path).chomp == '1'; end
+  def set_active_low(v = true); File.write(active_low_path, v ? '1':'0'); self; end
   class << self
     def poll_once(vio2gpio, timeout)
       _,_,ready = IO.select(nil, nil, vio2gpio.keys, timeout)
@@ -86,9 +50,9 @@ class GPIO
     self.class.poll([self], timeout:timeout, ready:ready_wrapper) do |v|
       v = v[self] if v
       if block
-        block.call v
+        block.call(v)
       else
-        break v
+        break(v)
       end
     end
   end
