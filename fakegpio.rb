b93890595de81caa155b98fc2cffdfd6665f12aa
Braden Pellett <contact@dabrado.net>
2014-03-15 11:31:21 -0700

    Change method for tracking edges that behaves more like a real GPIO

--- fakegpio.rb
+++ fakegpio.rb
@@ -5,26 +5,27 @@ class FakeGPIO
   def initialize(id)
     @redis = Redis.new
     @id = id
-    @triggered = true
+    @edge_count = nil
   end
+  def selected?; chkexp; @edge_count!=@redis.get(rkey(:edge_count)); end
   def rkey(name); "gpio:#{@id}:#{name}"; end
   def chmod(_); self; end
   def chown(_,_); self; end
   def exported?; @redis.exists(rkey(:value)); end
   def chkexp; raise "Not exported" if !exported?; end
-  def export; @redis.mset(rkey(:value),"\0",rkey(:direction),:in,rkey(:edge),:none,rkey(:active_low),"\0"); self; end
-  def unexport; chkexp; @redis.del(rkey(:value),rkey(:direction),rkey(:edge),rkey(:active_low)); self; end
+  def export; @redis.mset(rkey(:value),"\0",rkey(:direction),:in,rkey(:edge),:none,rkey(:active_low),"\0",rkey(:edge_count),'0'); self; end
+  def unexport; chkexp; @redis.del(rkey(:value),rkey(:direction),rkey(:edge),rkey(:active_low),rkey(:edge_count)); self; end
   def input?; chkexp; @redis.get(rkey(:direction)).to_sym==:in; end
   def output?; chkexp; @redis.get(rkey(:direction)).to_sym==:out; end
   def set_input; chkexp; @redis.set(rkey(:direction),:in); end
   def set_output_low(v=true); chkexp; @redis.set(rkey(:direction),:out); self.set_low(v); end
   def set_output_high(v=true); set_output_low(!v); end
-  def low?; chkexp; @triggered=false; @redis.getbit(rkey(:value),0)==0; end
+  def low?; chkexp; @edge_count=@redis.get(rkey(:edge_count)); @redis.getbit(rkey(:value),0)==0; end
   def high?; !low?; end
   def set_low(v=true); chkexp
     new = v ? 0 : 1
     old = @redis.setbit(rkey(:value),0,new)
-    @redis.publish('gpio:edge',@id) if
+    (@redis.incr(rkey(:edge_count)); @redis.publish('gpio:edge',@id)) if
       ( old != new && self.edge_both? ) ||
       ( old == 0 && new == 1 && self.edge_rising? ) ||
       ( old == 1 && new == 0 && self.edge_falling? )
@@ -41,16 +42,13 @@ class FakeGPIO
     def select(gpios, timeout:nil)
       redis = Redis.new
       begin
-        selected = gpios.select{|g|g.instance_variable_get('@triggered')}
+        selected = gpios.select{|g|g.selected?}
         return selected if selected != []
-        Timeout.timeout(timeout){redis.subscribe('gpio:edge'){|s|s.message{|_,id|
-          selected = gpios.find{|g|g.id==id.to_i}
-          if selected
-            selected.instance_variable_set('@triggered', true)
-            redis.unsubscribe
-          end
+        Timeout.timeout(timeout){redis.subscribe('gpio:edge'){|s|s.message{
+          selected = gpios.select{|g|g.selected?}
+          redis.unsubscribe if selected
         }}}
-        [selected]
+        selected
       rescue Timeout::Error
         nil
       end
